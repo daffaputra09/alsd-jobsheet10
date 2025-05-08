e62a19bf88a90e43a654796734c3af8ac2b7e2e0
daffaputra09 <daffaputra863@gmail.com>
2025-05-08 15:00:22 +0700

    feat: menambahkan method LihatAkhir

--- AntrianLayanan.java
+++ AntrianLayanan.java
@@ -67,6 +67,16 @@ public class AntrianLayanan {
         }
     }
 
+    public void LihatAkhir() {
+        if (isEmpty()) {
+            System.out.println("Antrian kosong.");
+        } else {
+            System.out.println("Mahasiswa paling belakang: ");
+            System.out.println("NIM - NAMA - PRODI - KELAS");   
+            data[rear].tampilkanData();
+        }
+    }
+
     public int getJumlahAntrian() {
         return size;
     }
